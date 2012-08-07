75db180ab50736c9bd7f912d2b89bee6eaa5bfe3
Anders Carling <anders.carling@d05.se>
2012-08-07 21:20:29 +0200

    Adding first implementation of countdown

--- js/countdown.js
+++ js/countdown.js
@@ -3,6 +3,34 @@
 // var views  = sp.require('sp://import/scripts/api/views');
 // var auth   = sp.require('sp://import/scripts/api/auth');
 
-exports.run = function(maindiv, callback) {
-    setTimeout(function() { callback(); }, 5000);
+var callback, default_fontsize, default_opacity;
+
+var cntdiv = $('<div id="countdown"/>');
+var cnt = 5;
+
+var tick = function() {
+
+    if (cnt > 0) {
+        cntdiv.css({'font-size': default_fontsize, 'opacity': default_opacity});
+        cntdiv.html(cnt);
+        cnt = cnt - 1;
+
+        cntdiv.transition({ 'font-size': '60pt' }, 800, function() {
+            cntdiv.transition({ 'opacity': 0 }, 200, function() {
+                tick();
+            });
+        });
+    } else {
+        callback();
+    }
+}
+
+exports.run = function(maindiv, cbk) {
+    callback = cbk;
+
+    maindiv.html(cntdiv)
+    default_fontsize = cntdiv.css('font-size');
+    default_opacity  = cntdiv.css('opacity');
+
+    tick();
 }
