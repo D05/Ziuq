bdba4c5aed4e4e13152ccb04aff27f39fc9d794d
Richard Henriksson <richen@student.chalmers.se>
2012-08-08 01:14:54 +0200

    question markup

--- js/quizzer.js
+++ js/quizzer.js
@@ -1,9 +1,11 @@
-// var sp = getSpotifyApi();
+var sp = getSpotifyApi();
 // var models = sp.require('sp://import/scripts/api/models');
 // var views  = sp.require('sp://import/scripts/api/views');
 // var auth   = sp.require('sp://import/scripts/api/auth');
+var template = sp.require('/js/template');
 
 exports.run = function(maindiv, quiz, cbk) {
     // mocked
-    cbk(quiz, 10, 7, 3);
+    //cbk(quiz, 10, 7, 3);
+	template.fetchInto(maindiv, "question", function (){});
 }
