f12b7204a17a37dee11ecc7cf03345dbc0e7bacd
Anders Carling <anders.carling@d05.se>
2012-08-08 00:17:00 +0200

    Adding initial implementation of killscreen

--- js/kill_screen.js
+++ js/kill_screen.js
@@ -1,8 +1,16 @@
-// var sp = getSpotifyApi();
+var sp = getSpotifyApi();
 // var models = sp.require('sp://import/scripts/api/models');
 // var views  = sp.require('sp://import/scripts/api/views');
 // var auth   = sp.require('sp://import/scripts/api/auth');
 
+var template = sp.require('js/template');
+
 exports.run = function(maindiv, results, cbk) {
-    cbk();
+    console.log(maindiv);
+    template.fetchInto(maindiv, 'kill_screen', function() {
+       $('#quiz_name').html(results.quiz.name);
+       $('#cntTotal').html(results.cntTotal);
+       $('#cntIncorrect').html(results.cntIncorrect);
+       $('#cntCorrect').html(results.cntCorrect);
+    });
 }
