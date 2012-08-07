870646e1566475fc2b560900be58197eebc04656
Anders Carling <anders.carling@d05.se>
2012-08-07 19:27:22 +0200

    Intial commit

---
+++ js/ziuq.js
@@ -0,0 +1,8 @@
+// var sp = getSpotifyApi();
+// var models = sp.require('sp://import/scripts/api/models');
+// var views  = sp.require('sp://import/scripts/api/views');
+// var auth   = sp.require('sp://import/scripts/api/auth');
+
+exports.init = function() {
+    console.log("init");
+}
