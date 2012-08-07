3ac1d6645ea284fe9e9d03e55d3ffd92a4bb0e60
Anders Carling <anders.carling@d05.se>
2012-08-07 20:18:26 +0200

    Adding auth and countdown modules

---
+++ js/authentication.js
@@ -0,0 +1,8 @@
+// var sp = getSpotifyApi();
+// var models = sp.require('sp://import/scripts/api/models');
+// var views  = sp.require('sp://import/scripts/api/views');
+// var auth   = sp.require('sp://import/scripts/api/auth');
+
+exports.run = function(maindiv, callback) {
+    callback("dummy token");
+}
--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,10 @@
 var sp = getSpotifyApi();
-// var models = sp.require('sp://import/scripts/api/models');
-// var views  = sp.require('sp://import/scripts/api/views');
+var models = sp.require('sp://import/scripts/api/models');
+var views  = sp.require('sp://import/scripts/api/views');
 // var auth   = sp.require('sp://import/scripts/api/auth');
+
+var authentication = sp.require('js/authentication');
+var countdown      = sp.require('js/countdown');
 
 var maindiv;
 
@@ -9,5 +12,10 @@ var maindiv;
 exports.run = function(el) {
     maindiv = $(el);
 
-    maindiv.append("We're up!");
+    authentication.run(maindiv, function (fbToken) {
+        countdown.run(maindiv, function() {
+            maindiv.html("now we run the quiz");
+        });
+    });
+
 }
