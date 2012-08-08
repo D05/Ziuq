7b430da0c171c78320288325a018261a9c79c7b1
Richard Henriksson <richen@student.chalmers.se>
2012-08-08 02:46:46 +0200

    Coundown markup

--- js/countdown.js
+++ js/countdown.js
@@ -1,36 +1,45 @@
-// var sp = getSpotifyApi();
+var sp = getSpotifyApi();
 // var models = sp.require('sp://import/scripts/api/models');
 // var views  = sp.require('sp://import/scripts/api/views');
 // var auth   = sp.require('sp://import/scripts/api/auth');
 
 var callback, default_fontsize, default_opacity;
 
-var cntdiv = $('<div id="countdown"/>');
-var cnt = 5;
+var template = sp.require('/js/template');
+
+var cntdiv;
+var cnt = 3;
 
 var tick = function() {
 
     if (cnt > 0) {
-        cntdiv.css({'font-size': default_fontsize, 'opacity': default_opacity});
-        cntdiv.html(cnt);
-        cnt = cnt - 1;
-
-        cntdiv.transition({ 'font-size': '60pt' }, 800, function() {
-            cntdiv.transition({ 'opacity': 0 }, 200, function() {
-                tick();
-            });
-        });
+        //cntdiv.css({'font-size': default_fontsize, 'opacity': default_opacity});
+        //cntdiv.html(cnt);
+        
+		cntdiv.html(cnt);
+		cnt = cnt - 1;
+		
+        setTimeout(function() {
+			tick();
+	
+		}, 1000);
+        
     } else {
         callback();
     }
 }
 
-exports.run = function(maindiv, cbk) {
+exports.run = function(maindiv, quiz, cbk) {
     callback = cbk;
 
-    maindiv.html(cntdiv)
-    default_fontsize = cntdiv.css('font-size');
-    default_opacity  = cntdiv.css('opacity');
+	template.fetchInto(maindiv, "countdown", function(){
+		$('#quiz_name').attr('src', quiz.image);
+		cntdiv = $("#countDownNumber");
+		
+	    //default_fontsize = cntdiv.css('font-size');
+	    //default_opacity  = cntdiv.css('opacity');
 
-    tick();
+	    tick();
+	});
+	
 }
